0f04e675d0ee84427a5214cff298caf479300aed
nikita.aleksandrov <mathymath24@gmail.com>
2022-05-29 12:28:09 +0200

    Refactored zipper

--- loader/zipProject.py
+++ loader/zipProject.py
@@ -12,32 +12,6 @@ class Zipper:
         self.base_dir = os.path.join(loader.currentPath, '..')
         self.path = os.path.join(self.base_dir, self.projectName + f'_v{self.current_version}')
 
-    def copy_required_files(self):
-        for file in self.filesToZip:
-            self.copySingleElement(file)
-
-    def copySingleElement(self, element):
-        typeOfElement = self.define_folder_or_file(element)
-        if typeOfElement == 'file':
-            self.copySingleFile(element)
-        if typeOfElement == 'folder':
-            self.copySingleFolder(element)
-
-    def define_folder_or_file(self, element):
-        if '.' in element:
-            return 'file'
-
-        return 'folder'
-
-    def copySingleFile(self, element):
-        base_path = os.path.join(self.base_dir, element)
-        shutil.copy(base_path, f'{self.path}')
-
-    def copySingleFolder(self, element):
-        fromPath = os.path.join(self.base_dir, element)
-        targetPath = os.path.join('..', self.path, element)
-        copy_tree(fromPath, targetPath)
-
     def create_new_version_zip(self):
         if not os.path.exists(self.path):
             os.mkdir(self.path)
@@ -47,11 +21,39 @@ class Zipper:
         else:
             self.increase_version()
             self.set_new_path()
+
             self.create_new_version_zip()
+
+    def copy_required_files(self):
+        for file in self.filesToZip:
+            self.copy_single_element(file)
+
+    def copy_single_element(self, element):
+        typeOfElement = self.define_folder_or_file(element)
+        if typeOfElement == 'file':
+            self.copy_single_file(element)
+        if typeOfElement == 'folder':
+            self.copy_single_folder(element)
+
+    @staticmethod
+    def define_folder_or_file(element):
+        if '.' in element:
+            return 'file'
+
+        return 'folder'
+
+    def copy_single_file(self, element):
+        base_path = os.path.join(self.base_dir, element)
+        shutil.copy(base_path, f'{self.path}')
+
+    def copy_single_folder(self, element):
+        fromPath = os.path.join(self.base_dir, element)
+        targetPath = os.path.join('..', self.path, element)
+        copy_tree(fromPath, targetPath)
 
     def make_zip(self):
         outputFileName = self.projectName + f'_{self.current_version}'
-        out = os.path.join(self.base_dir, outputFileName, '..', outputFileName)
+        out = os.path.join(self.base_dir, outputFileName)
         shutil.make_archive(out, 'zip', self.path)
 
     def increase_version(self):
